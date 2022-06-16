044daf31914facfb611e81290c5666417abfe246
eskokado <eskokado@gmail.com>
2022-06-16 16:40:23 -0300

    feat: multi instence

--- Program.cs
+++ Program.cs
@@ -3,8 +3,10 @@ class Program {
   static void Main(string[] args) 
   {
     Arus hero = new Arus("Arus", 23, "Knight");
+    Arus wizard = new Arus("Jerônica", 23, "White Wizard"); 
 
-    Console.Write(hero);
+    Console.WriteLine(hero.Attack());
+    Console.WriteLine(wizard.Attack());
   }
 }
 
--- src/entities/Arus.cs
+++ src/entities/Arus.cs
@@ -23,5 +23,9 @@ namespace game_jpg.src.entities
         public override string ToString() {
             return $"Name: {this.Name} - Level: {this.Level} - Type: {this.HeroType}";
         }
+
+        public string Attack() {
+            return $"{this.Name} atacou com a sua espada"; 
+        }
     }
 }
