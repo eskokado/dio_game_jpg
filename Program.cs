654c7dcc31bc79a8e5d226b9eda4d10f63d6f90f
eskokado <eskokado@gmail.com>
2022-06-16 16:00:11 -0300

    feat: definity obj arus

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ class Program {
   static void Main(string[] args) 
   {
     Arus hero = new Arus();
+    hero.Name = "Arus";
+    hero.Level = 2;
+    hero.HeroType = "Knight";
 
     Console.Write("");
   }
